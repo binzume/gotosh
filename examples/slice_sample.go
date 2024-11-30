7d10b50242ac2240eff0a29ca6afd78bf02416dc
Kousuke Kawahira <kawahira@binzume.net>
2024-11-30 17:23:56 +0900

    update slice sample

--- examples/slice_sample.go
+++ examples/slice_sample.go
@@ -4,6 +4,10 @@ import (
 	"fmt"
 )
 
+func printSlice(msg string, s []int) {
+	fmt.Println(msg, "len:", len(s), "values:", s)
+}
+
 func getSlice() []int {
 	a := []int{1, 2, 3, 4}
 	return a
@@ -13,13 +17,11 @@ func main() {
 	var a = []int{1, 2, 3, 4}
 	a = append(a, 123, 456, 789)
 	a = append(a, 111, 444, 777)
-	fmt.Println("len", len(a))
-	for i := 0; i < len(a); i++ {
-		fmt.Println(i, a[i])
-	}
-	fmt.Println("slice", a[1:2])
+	printSlice("a:", a)
+	a = getSlice()
+	printSlice("a:", a)
 
-	// range not supported yet
+	// range is not supported yet
 	// for i, v := range a {
 	//	fmt.Println(i, v)
 	//}
@@ -30,11 +32,6 @@ func main() {
 	for i := 0; i < len(ss); i++ {
 		fmt.Println(i, ss[i])
 	}
-
-	fmt.Println(a)
-	fmt.Println(ss)
-	a = getSlice()
-	fmt.Println(a)
 
 	s := "abcdefghijk"
 	fmt.Println(s[1+2 : 5+3])
