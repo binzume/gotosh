0602b25e21195d094c50f35bb1be02dbac5ed5d6
Kousuke Kawahira <kawahira@binzume.net>
2024-11-30 17:10:04 +0900

    update fizz buzz

--- examples/fizz_buzz.go
+++ examples/fizz_buzz.go
@@ -8,7 +8,7 @@ const buzz = "Buzz"
 func FizzBuzz(n int) {
 	for i := 1; i <= n; i++ {
 		if i%15 == 0 {
-			fmt.Println(fizz, buzz)
+			fmt.Println(fizz + buzz)
 		} else if i%3 == 0 {
 			fmt.Println(fizz)
 		} else if i%5 == 0 {
