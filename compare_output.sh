1dd167db3b2b239d9e35d56962a37d2faf960915
Kousuke Kawahira <kawahira@binzume.net>
2024-12-01 09:29:43 +0900

    test script

--- compare_output.sh
+++ compare_output.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
+IN=go.mod
+OUT_DIR=./out
 
+mkdir -p $OUT_DIR
 echo "============ Go ============"
-go run examples/$1.go | tee $1.out_go
+go run examples/$1.go < $IN | tee $OUT_DIR/$1.out_go
 echo "============ Bash ============"
-go run . examples/$1.go > $1.sh
-bash $1.sh 2>&1 | tee $1.out_sh
+go run . examples/$1.go > $OUT_DIR/$1.sh
+bash $OUT_DIR/$1.sh 2>&1 < $IN | tee $OUT_DIR/$1.out_sh
 echo "============ Diff ============"
-diff $1.out_go $1.out_sh
+diff $OUT_DIR/$1.out_go $OUT_DIR/$1.out_sh
 echo "============ $1 finished ============"
