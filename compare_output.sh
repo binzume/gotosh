5922fc8feec9212e15338b391a78d243ad30371e
Kousuke Kawahira <kawahira@binzume.net>
2024-12-02 13:16:14 +0900

    Add gh actions

--- compare_output.sh
+++ compare_output.sh
@@ -3,11 +3,12 @@ IN=go.mod
 OUT_DIR=./out
 
 mkdir -p $OUT_DIR
-echo "============ Go ============"
-go run examples/$1.go < $IN | tee $OUT_DIR/$1.out_go
-echo "============ Bash ============"
 go run . examples/$1.go > $OUT_DIR/$1.sh
+go run examples/$1.go < $IN > $OUT_DIR/$1.out_go
+echo "============ $1.sh ============"
 bash $OUT_DIR/$1.sh 2>&1 < $IN | tee $OUT_DIR/$1.out_sh
 echo "============ Diff ============"
 diff $OUT_DIR/$1.out_go $OUT_DIR/$1.out_sh
+ret=$?
 echo "============ $1 finished ============"
+exit $ret
