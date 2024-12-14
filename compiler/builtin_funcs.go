ae493862e99c9efc185e52b69fc27fde3a7dd11e
Kousuke Kawahira <kawahira@binzume.net>
2024-12-14 10:52:50 +0900

    separate builtin funcs

---
+++ compiler/builtin_funcs.go
@@ -0,0 +1,96 @@
+package compiler
+
+import (
+	"strings"
+)
+
+// TODO: export types to modify from outside
+var InitBuiltInFuncs = func(s *state) {
+	s.funcs = map[string]shFunc{
+		"shell.Sleep":      {exp: "sleep"},
+		"shell.Exit":       {exp: "exit"},
+		"shell.Export":     {exp: "export"},
+		"shell.Exec":       {retTypes: []Type{"string", "StatusCode"}, stdout: true},
+		"shell.Read":       {exp: `IFS= read -r -s _tmp0`, retTypes: []Type{"TempVarString", "StatusCode"}},
+		"shell.ReadLine":   {exp: `IFS= read -r -s _tmp0 <&{0}`, retTypes: []Type{"TempVarString", "StatusCode"}},
+		"shell.SubStr":     {exp: "\"${{*0}:{1}:{2}}\"", retTypes: []Type{"string"}},
+		"shell.Arg":        {exp: `eval echo \${{0}}`, retTypes: []Type{"string"}, stdout: true},
+		"shell.NArgs":      {exp: `$(( $# + 1 ))`, retTypes: []Type{"int"}},
+		"shell.UnixTimeMs": {exp: `printf '%.0f' $( echo "${EPOCHREALTIME:-$(date +%s)} * 1000" | bc )`, retTypes: []Type{"int"}, stdout: true},
+		"shell.Do":         {convFunc: func(arg []string) string { return trimQuote(arg[0]) }},
+		// fmt
+		"fmt.Print":   {exp: "echo -n"},
+		"fmt.Println": {exp: "echo"},
+		"fmt.Printf":  {exp: "printf"},
+		"fmt.Sprint":  {exp: "echo -n", retTypes: []Type{"string"}, stdout: true},
+		"fmt.Sprintln": {retTypes: []Type{"string"}, convFunc: func(arg []string) string {
+			return "$(echo " + strings.Join(arg, " ") + ")$'\\n'"
+		}},
+		"fmt.Sprintf":  {exp: "printf", retTypes: []Type{"string"}, stdout: true},
+		"fmt.Fprint":   {convFunc: func(arg []string) string { return "echo -n " + strings.Join(arg[1:], " ") + " >&" + arg[0] }},
+		"fmt.Fprintln": {convFunc: func(arg []string) string { return "echo " + strings.Join(arg[1:], " ") + " >&" + arg[0] }},
+		"fmt.Fprintf":  {convFunc: func(arg []string) string { return "printf " + strings.Join(arg[1:], " ") + " >&" + arg[0] }},
+		// strings
+		"strings.ReplaceAll": {exp: "\"${{*0}//{1}/{2}}\"", retTypes: []Type{"string"}},
+		"strings.ToUpper":    {exp: "echo {0}|tr '[:lower:]' '[:upper:]'", retTypes: []Type{"string"}, stdout: true},
+		"strings.ToLower":    {exp: "echo {0}|tr '[:upper:]' '[:lower:]'", retTypes: []Type{"string"}, stdout: true},
+		"strings.TrimSpace":  {exp: "echo {0}| sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//'", retTypes: []Type{"string"}, stdout: true},
+		"strings.TrimPrefix": {exp: "\"${{*0}#{1}}\"", retTypes: []Type{"string"}},
+		"strings.TrimSuffix": {exp: "\"${{*0}%{1}}\"", retTypes: []Type{"string"}},
+		"strings.Split": {retTypes: []Type{"[]string"}, stdout: true, convFunc: func(arg []string) string {
+			return "IFS=" + arg[1] + " _tmp0=(" + trimQuote(arg[0]) + ") ;echo \"${_tmp0[@]}\""
+		}},
+		"strings.Join":     {exp: "IFS={1}; echo \"${{*0}[*]}\"", retTypes: []Type{"string"}, stdout: true},
+		"strings.Contains": {exp: "case {0} in *{1}* ) echo 1;; *) echo 0;; esac", retTypes: []Type{"bool"}, stdout: true},
+		"strings.IndexAny": {exp: "expr '(' index {0} {1} ')' - 1", retTypes: []Type{"int"}, stdout: true},
+		// os
+		"os.Stdin":    {exp: "0", retTypes: []Type{"*os.File"}}, // variable
+		"os.Stdout":   {exp: "1", retTypes: []Type{"*os.File"}}, // variable
+		"os.Stderr":   {exp: "1", retTypes: []Type{"*os.File"}}, // variable
+		"os.Exit":     {exp: "exit"},
+		"os.Getwd":    {exp: "pwd", retTypes: []Type{"string", "StatusCode"}, stdout: true},
+		"os.Chdir":    {exp: "cd", retTypes: []Type{"StatusCode"}, stdout: true},
+		"os.Getpid":   {exp: "$$", retTypes: []Type{"int"}},
+		"os.Getppid":  {exp: "$PPID", retTypes: []Type{"int"}},
+		"os.Getuid":   {exp: "${UID:--1}", retTypes: []Type{"int"}},
+		"os.Geteuid":  {exp: "${EUID:-${UID:--1}}", retTypes: []Type{"int"}},
+		"os.Getgid":   {exp: "${GID:--1}", retTypes: []Type{"int"}},
+		"os.Getegid":  {exp: "${EGID:-${GID:--1}}", retTypes: []Type{"int"}},
+		"os.Hostname": {exp: "uname -n", retTypes: []Type{"string", "StatusCode"}, stdout: true},
+		"os.Getenv": {convFunc: func(arg []string) string {
+			return "\"${" + trimQuote(arg[0]) + "}\""
+		}, retTypes: []Type{"string"}},
+		"os.Setenv": {convFunc: func(arg []string) string {
+			return "export " + trimQuote(arg[0]) + "=" + arg[1]
+		}},
+		"os.Pipe":              {exp: `_tmp=$(mktemp -d) && mkfifo $_tmp/f && _tmp0=$(( ++GOTOSH_fd + 2 )) && _tmp1=$(( ++GOTOSH_fd + 2 )) && eval "exec $_tmp1<>\"$_tmp/f\" $_tmp0<\"$_tmp/f\"" && rm -rf $_tmp`, retTypes: []Type{"*os.File", "*os.File", "StatusCode"}},
+		"os.Open":              {exp: `_tmp0=$(( ++GOTOSH_fd + 2 )) ; eval "exec $_tmp0<{0}"`, retTypes: []Type{"*os.File", "StatusCode"}},
+		"os.Create":            {exp: `_tmp0=$(( ++GOTOSH_fd + 2 )) ; eval "exec $_tmp0>{0}"`, retTypes: []Type{"*os.File", "StatusCode"}},
+		"os.Mkdir":             {exp: "mkdir {0}", retTypes: []Type{"StatusCode"}},
+		"os.MkdirAll":          {exp: "mkdir -p {0}", retTypes: []Type{"StatusCode"}},
+		"os.Remove":            {exp: "rm -f", retTypes: []Type{"StatusCode"}},
+		"os.RemoveAll":         {exp: "rm -rf", retTypes: []Type{"StatusCode"}},
+		"os.Rename":            {exp: "mv", retTypes: []Type{"StatusCode"}},
+		"os.File__WriteString": {exp: `echo -n {1} >&{0}`},
+		"os.File__Close":       {exp: `eval "exec {0}<&- {0}>&-"`},
+		"exec.Command":         {exp: "echo -n ", retTypes: []Type{"*exec.Cmd"}, stdout: true}, // TODO escape command string...
+		"exec.Cmd__Output":     {exp: "bash -c", retTypes: []Type{"string", "StatusCode"}, stdout: true},
+		"reflect.TypeOf":       {retTypes: []Type{"string"}, convFunc: func(arg []string) string { return `"` + string(s.vars[varName(arg[0])]) + `"` }},
+		"runtime.Compiler":     {exp: "'gotosh'", retTypes: []Type{"string"}},               // constant
+		"runtime.GOARCH":       {exp: "uname -m", retTypes: []Type{"string"}, stdout: true}, // constant
+		"runtime.GOOS":         {exp: "uname -o", retTypes: []Type{"string"}, stdout: true}, // constant
+		// TODO: cast
+		"int":              {retTypes: []Type{"int"}},
+		"byte":             {retTypes: []Type{"int"}},
+		"string":           {retTypes: []Type{"string"}},
+		"strconv.Atoi":     {retTypes: []Type{"int", "StatusCode"}},
+		"strconv.Itoa":     {retTypes: []Type{"string"}},
+		"shell.StatusCode": {retTypes: []Type{"int"}},
+		// slice
+		"len": {retTypes: []Type{"int"}, convFunc: func(arg []string) string { return "${#" + strings.Trim(trimQuote(arg[0]), "${}") + "}" }},
+		"append": {retTypes: []Type{"_ARG1"},
+			convFunc: func(arg []string) string {
+				return varName(arg[0]) + "+=(" + strings.Join(arg[1:], " ") + ")"
+			}},
+	}
+}
