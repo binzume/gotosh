91169a4e520aa269894b5758196dbadb12577719
Kousuke Kawahira <kawahira@binzume.net>
2024-12-21 22:21:08 +0900

    remove -u

--- compare_output.sh
+++ compare_output.sh
@@ -7,7 +7,7 @@ mkdir -p $OUT_DIR
 go run . examples/$1.go > $OUT_DIR/$1.sh
 go run examples/$1.go $PARAMS < $IN > $OUT_DIR/$1.out_go
 echo "============ $1.sh ============"
-bash -u $OUT_DIR/$1.sh $PARAMS 2>&1 < $IN | tee $OUT_DIR/$1.out_sh
+bash $OUT_DIR/$1.sh $PARAMS 2>&1 < $IN | tee $OUT_DIR/$1.out_sh
 echo "============ Diff ============"
 diff $OUT_DIR/$1.out_go $OUT_DIR/$1.out_sh
 ret=$?
